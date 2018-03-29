dc5e5ed6641a9183f4321dc64bd17478b5e04dbb
Pherang <pherang@users.noreply.github.com>
2018-03-29 00:56:36 -0400

    Used pug to create main page

---
+++ fileupload.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+      //filesystem = require('file-system'),
+      app = express()
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.get('/', (req,res) => {
+  
+  res.render('index', { title: 'Hello!', message: 'Choose a file to upload'} )
+
+})
+
+app.post('/', (req,res) => {
+  res.send('You posted')
+}
+
+app.listen(8080, () => {
+  console.log('Server listening on 8080')
+})
